23c502b089b44f9d715ce8c20a9a1eab9e2f3c1c
Svvarg <stizerblack@gmail.com>
2019-10-28 19:56:51 +0200

    Added Vanila Armor

--- src/main/java/com/svvarg/sundry/Sundry.java
+++ src/main/java/com/svvarg/sundry/Sundry.java
@@ -15,6 +15,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
 import net.minecraftforge.common.util.EnumHelper;
 import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
 
 @Mod(modid = Sundry.MODID, version = Sundry.VERSION)
 public class Sundry {
@@ -24,37 +25,51 @@ public class Sundry {
     public static Item key;
     public static Item samdust;
     private static Item samingot;
-    
+
     private static ItemFood berry;
-    
+
     public static Item titaniumpickaxe;
     ToolMaterial titanium = EnumHelper.addToolMaterial("titanium", 3, 1000, 9.5F, 3.5F, 10);
-    
+
     private static Item sampaxel;
-    
+
+    //armor
+    public static Item shelmet;
+    public static Item schest;
+    public static Item sleggings;
+    public static Item sboots;
+
+    ArmorMaterial sarmor = EnumHelper.addArmorMaterial("sarmor", 20, new int[]{3, 7, 6, 3}, 10);
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        key = new ItemKey();        
+        key = new ItemKey();
         GameRegistry.registerItem(key, "Key");
-        
+
         samdust = new ItemSamGeneric("samdust");
         samingot = new ItemSamGeneric("samingot");
         GameRegistry.registerItem(samdust, "SamDust");
         GameRegistry.registerItem(samingot, "SamIngot");
-        
+
         berry = new ItemBerry(3, 0.3F, true, "Berry");
         GameRegistry.registerItem(berry, "Berry");
-        
+
         titaniumpickaxe = new ItemTitaniumPickaxe(titanium, "titaniumpickaxe");
         GameRegistry.registerItem(titaniumpickaxe, "TitaniumPickaxe");
-        
-        sampaxel = new ItemSamPaxel (titanium, "sampaxel");
-        GameRegistry.registerItem(sampaxel, "SamsPaxel");
-        
-    }
 
-    
+        sampaxel = new ItemSamPaxel(titanium, "sampaxel");
+        GameRegistry.registerItem(sampaxel, "SamsPaxel");
+
+        shelmet = new ItemSArmor(sarmor, 0, "shelmet");
+        schest = new ItemSArmor(sarmor, 1, "schestplate");
+        sleggings = new ItemSArmor(sarmor, 2, "sleggings");
+        sboots = new ItemSArmor(sarmor, 3, "sboots");
+        GameRegistry.registerItem(shelmet, "SHelmet");
+        GameRegistry.registerItem(schest, "SChest");
+        GameRegistry.registerItem(sleggings, "SLeggings");
+        GameRegistry.registerItem(sboots, "SBooths");
+
+    }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
@@ -65,6 +80,6 @@ public class Sundry {
         ItemStack enchantedSwordItemStack = new ItemStack(Items.stone_sword);
         enchantedSwordItemStack.addEnchantment(Enchantment.sharpness, 1);
         GameRegistry.addShapelessRecipe(enchantedSwordItemStack, Items.flint, Items.stone_sword);
-    
+
     }
 }
